9350b702670e00954217e4345a0a2f00f5db407a
Hans-Joerg Schurr <commits@schurr.at>
2016-01-22 15:34:19 +0100

    bug fixes

--- analyze/src/Frameworks.hs
+++ analyze/src/Frameworks.hs
@@ -1,28 +1,29 @@
-module Semantics (Extrensions) where
+module Semantics (Framework) where
 
 import Data.Set
 import qualified Data.Map.Strict as M
 
-import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy.Char8 as B
 import Text.Regex.Posix
 
 type Argument = B.ByteString
 
 -- A framework consists of a set of arguments and a map from arguments to
 -- arguments attacket by an argument.
-data Framework = Framework (Data.Set Argument) (Data.Map Argument [Argument])
+data Framework = Framework (Set Argument) (M.Map Argument [Argument])
 
-apxArg = L.pack "^arg\\([ \\t]*([a-z0-9]+)[ \\t]*\\)\\.$"
-apxAtt = L.pack "^att\\([ \\t]*([a-z0-9]+)[ \\t]*,[ \\t]*([a-z0-9]+)[ \\t]*\\)\\.$"
+apxArg = B.pack "^arg\\([ \\t]*([a-z0-9]+)[ \\t]*\\)\\.$"
+apxAtt = B.pack "^att\\([ \\t]*([a-z0-9]+)[ \\t]*,[ \\t]*([a-z0-9]+)[ \\t]*\\)\\.$"
 
 apxParseLine ∷ Framework → B.ByteString  → Framework
 apxParseLine f@(Framework args attacks) str =
   let
-    arg = str =~ apxArg
-    att = str =~ apxAtt
+    arg = str =~ apxArg ∷ [[B.ByteString]]
+    att = str =~ apxAtt ∷ [[B.ByteString]]
   in
     case arg of
-      [[_,arg]] → Framework (insert arg args) attacks
+      [[_,arg]] → Framework ns attacks
+        where ns = insert arg args
       _ → case att of
         [[_,arg1,arg2]] → Framework args (M.insertWith (++) arg1 [arg2] attacks)
         _ → f
