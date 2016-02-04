495f87e37ab35ca1aa023ff8e6b758c34dad0ec0
Hans-Joerg Schurr <commits@schurr.at>
2016-02-04 19:14:26 +0100

    Parsing APX and TGF files

--- analyze/src/Frameworks.hs
+++ analyze/src/Frameworks.hs
@@ -1,6 +1,6 @@
-module Semantics (Framework) where
+module Frameworks (Framework, readApx, readTgf) where
 
-import Data.Set
+import qualified Data.Set as S
 import qualified Data.Map.Strict as M
 
 import qualified Data.ByteString.Lazy.Char8 as B
@@ -10,7 +10,8 @@ type Argument = B.ByteString
 
 -- A framework consists of a set of arguments and a map from arguments to
 -- arguments attacket by an argument.
-data Framework = Framework (Set Argument) (M.Map Argument [Argument])
+data Framework = Framework (S.Set Argument) (M.Map Argument [Argument])
+  deriving Show
 
 apxArg = B.pack "^arg\\([ \\t]*([a-z0-9]+)[ \\t]*\\)\\.$"
 apxAtt = B.pack "^att\\([ \\t]*([a-z0-9]+)[ \\t]*,[ \\t]*([a-z0-9]+)[ \\t]*\\)\\.$"
@@ -23,7 +24,34 @@ apxParseLine f@(Framework args attacks) str =
   in
     case arg of
       [[_,arg]] → Framework ns attacks
-        where ns = insert arg args
+        where ns = S.insert arg args
       _ → case att of
         [[_,arg1,arg2]] → Framework args (M.insertWith (++) arg1 [arg2] attacks)
         _ → f
+
+readApx ∷ [B.ByteString] → Framework
+readApx = foldl apxParseLine (Framework S.empty M.empty)
+
+tgfArg = B.pack "([0-9]+).*$"
+tgfAtt = B.pack "([0-9]+)[ \\t]+([0-9]+).*$"
+tgfSep = B.pack "#"
+data  TgfState = Arguments | Attacks
+
+tgfParseLine ∷  (TgfState, Framework) → B.ByteString → (TgfState, Framework)
+tgfParseLine (Arguments, f@(Framework args attacks)) str
+  | str == tgfSep = (Attacks, f)
+  | otherwise =
+    let
+      [[_,arg]] = str =~ tgfArg ∷ [[B.ByteString]]
+      nargs = S.insert arg args
+    in
+      (Arguments, Framework nargs attacks)
+tgfParseLine (Attacks, f@(Framework args attacks)) str =
+  let
+    [[_,arg1, arg2]] = str =~ tgfAtt ∷ [[B.ByteString]]
+    natt = M.insertWith (++) arg1 [arg2] attacks
+  in
+    (Attacks, Framework args natt)
+
+readTgf ∷ [B.ByteString] → Framework
+readTgf = snd . foldl tgfParseLine (Arguments, Framework S.empty M.empty)
--- analyze/src/Main.hs
+++ analyze/src/Main.hs
@@ -1,1 +1,11 @@
-main = putStrLn "Hello, World!"
+import Frameworks
+import qualified Data.ByteString.Lazy.Char8 as B
+import System.IO
+
+main ∷ IO ()
+main = do
+  let filename = "test.tgf"
+  handle ← openFile filename ReadMode
+  content ←  B.hGetContents handle
+  let framework = readTgf $ B.lines content
+  print framework
